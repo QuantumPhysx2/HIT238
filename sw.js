c8da2ea8b4570b7e51482f764d46f10f098326a5
Gia Phu Tran <S280030@students.cdu.edu.au>
2018-08-24 15:06:56 +0930

    ServiceWorker Fetch v1.14

--- sw.js
+++ sw.js
@@ -22,7 +22,7 @@ self.addEventListener("fetch", function(event) {
 		caches.match(event.request)
 			.then(function(response) {
 				if (response) {
-					console.log("Fetch Success");
+					console.log("Offline Fetch Success! Your page now works offline");
 					return response;
 				}
 			return fetch(event.request);
