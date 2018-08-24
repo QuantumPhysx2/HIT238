9fd1a0656e7a8347b07cc3eaf363777ef9328b1f
Gia Phu Tran <S280030@students.cdu.edu.au>
2018-08-24 14:55:39 +0930

    ServiceWorker Fetch v1

--- sw.js
+++ sw.js
@@ -17,14 +17,14 @@ self.addEventListener("install", function(event) {
 });
 
 self.addEventListener("fetch", function(event) {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      }
-    )
-  );
+	event.respondWith(
+		caches.match(event.request)
+			.then(function(response) {
+				if (response) {
+					return response;
+				}
+			return fetch(event.request);
+			}
+		)
+	);
 });
