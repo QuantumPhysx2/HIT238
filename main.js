7109de5ff772edeba8c69778a103c75383535398
Gia Phu Tran <S280030@students.cdu.edu.au>
2018-08-24 14:45:17 +0930

    ServiceWorker stuffssssss

--- main.js
+++ main.js
@@ -1,22 +1,3 @@
-var CACHE_TITLE = 'my-site-cache';
-var CACHE_VERSION = 'v1';
-var CACHE_NAME = CACHE_TITLE + '-' + 'CACHE_VERSION';
-var urlsToCache = [
-  'css/Style.css',
-  'main.js'
-];
-
-self.addEventListener("install", function(event) {
-	event.waitUntil(
-		caches.open(CACHE_NAME)
-			.then(function(cache) {
-				console.log("Opened cache");
-				return cache.addAll(urlsToCache);
-			})
-	);
-});
-
-
 if ("serviceWorker" in navigator) {
 	window.addEventListener("load", function() {
 		navigator.serviceWorker.register("gimble.js").then(function(registration) {
