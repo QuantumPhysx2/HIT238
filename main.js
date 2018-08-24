c7d0ec95f64cd15a08f20098f452eb31fdf5a0c8
Gia Phu Tran <S280030@students.cdu.edu.au>
2018-08-24 14:33:21 +0930

    ServiceWorker stuffssss

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 if ("serviceWorker" in navigator) {
 	window.addEventListener("load", function() {
-		navigaator.serviceWorker.register("gimble.js").then(function(registration) {
+		navigator.serviceWorker.register("gimble.js").then(function(registration) {
 			console.log("Success", registration.scope);
 		}, function(err) {
 			console.log("Fail", err);
